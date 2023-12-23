8f161be1f58333d255f45dbcd1747e70bf7272e5
VICTOR <victor.kipkoech@student.moringaschool.com>
2023-12-23 12:56:48 +0300

    completed

--- lib/testing_env/module_test.py
+++ lib/testing_env/module_test.py
@@ -7,12 +7,14 @@ from versions import (
 def test_python_version():
     version_info = python_version()
     assert version_info.major == 3
-    assert version_info.minor == 8
+    assert version_info.minor == 10
+
+
 
 
 def test_requests_version():
-    assert requests_version() == "2.27.1"
+    assert requests_version() == "2.31.0"
 
 
 def test_pytest_version():
-    assert pytest_version() == "7.1.3"
+    assert pytest_version() == "7.4.3"
